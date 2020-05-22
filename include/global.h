056a95994bee06e3785cc785995194ade4e15c2c
qzt <qinzhtao9527@outlook.com>
2020-05-22 21:27:19 +0800

    整理代码

--- include/global.h
+++ include/global.h
@@ -13,48 +13,48 @@
 #include "cga.h"
 
 // gdt
-EXTERN	u8		gdt_ptr[6];
+EXTERN	u8		    gdt_ptr[6];
 EXTERN	DESCRIPTOR	gdt[GDT_SIZE];
 
 // idt
-EXTERN	u8		idt_ptr[6];
+EXTERN	u8		    idt_ptr[6];
 EXTERN	GATE		idt[IDT_SIZE];
 
 // 当前显示的行数
-EXTERN	u32		dis_pos;
+EXTERN	u32		    dis_pos;
 
 //
-EXTERN	PROCESS	proc_table[NR_TASKS];
+EXTERN	PROCESS	    proc_table[NR_TASKS];
 EXTERN  PROCESS*    cur_proc;
 
 //
 EXTERN	TSS	proc_tss;
 
 //irq中断向量
-EXTERN int_handler irq_table[IRQ_NUM];
+EXTERN int_handler  irq_table[IRQ_NUM];
 
 // 时钟计数
-EXTERN u32  ticks;
+EXTERN u32          ticks;
 
 // ARDS
-EXTERN ARDS*    p_ards;
+EXTERN ARDS*        p_ards;
 
 // ARDS数量
-EXTERN u8       n_ards;
+EXTERN u8           n_ards;
 
 // 内存总空间
-EXTERN u32     MemTotal;
+EXTERN u32          MemTotal;
 
 // 内存空余总空间
-EXTERN u32     MemFree;
+EXTERN u32          MemFree;
 
 // 记录第一个空闲的页表(虚拟地址),用于PTE或者PDE的页帧
-EXTERN FPAGE*   pFreePage;
+EXTERN FPAGE*       pFreePage;
 //记录页目录的地址(物理地址)
-EXTERN PDE*     pPDETable;
+EXTERN PDE*         pPDETable;
 
 // 
 EXTERN  BUDDYBLOCK* pBuddyBlocks[11];
 
 // 指向显存地址
-EXTERN GMCHAR   (*pVGAMEM)[CGA_CHARS_LINE];
+EXTERN GMCHAR       (*pVGAMEM)[CGA_CHARS_LINE];
