6ff822abe9c3c8ad1bd8d7758a1df5226e1668bb
qzt <qinzhtao9527@outlook.com>
2020-05-22 22:59:34 +0800

    调整代码

--- kernel/start.c
+++ kernel/start.c
@@ -13,6 +13,9 @@ PUBLIC	void	cstart(){
 	u32 *iptr_base  = (u32*)(&idt_ptr[2]);
 	*iptr_limit = IDT_SIZE * sizeof(GATE) - 1;
 	*iptr_base  = (u32)idt;
+	
+	// 初始化中断和异常
+	init_prot();
 
 	// 加载idt
 	__asm__(
@@ -29,8 +32,6 @@ PUBLIC	void	cstart(){
 
 	// char *strng = "123\n456\n789";
 	// display_str_colorful(strng, 0x74);
-	// 初始化中断和异常
-	init_prot();
 
 	//初始化多进程
 	init_multiproc();
